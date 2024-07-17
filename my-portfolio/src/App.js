f98b29f105fb52aedfd7870a20aee9fa5abeb5f4
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-17 10:46:42 +0200

    Create component files and integrate in App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './App.css'
+import About from './components/About';
+import NavBar from './components/NavBar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      My Portfolio Website
+      <main className='text-gray-400 bg-gray-900 body-font'>
+        <NavBar />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
     </div>
   );
 }
