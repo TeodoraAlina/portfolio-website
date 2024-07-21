6b26150ae32a6b4efc4f84145a233494c14f649d
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-21 08:32:34 +0200

    Add content and styles to the Projects component

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center font-roboto">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-extrabold text-primary font-roboto">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-extrabold text-primary">
             Hi, I'm Alina Teodora.
             <br className="lg:inline-block" />Lorem ipsum dolor sit amet.
           </h1>
--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { projects } from '../data/data';
+import { CodeIcon } from '@heroicons/react/solid';
 
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <section id="projects" className="text-primary body-font font-roboto bg-gray-300">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <CodeIcon className="mx-auto inline-block w-10 mb-4 text-primary" />
+          <h1 className="sm:text-4xl text-3xl font-extrabold title-font mb-4 text-primary font-roboto">
+            Projects I've Built
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-secondary">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
+            fuga dolore.
+          </p>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              className="sm:w-1/2 w-full p-4">
+              <div className="relative h-64 overflow-hidden rounded-lg shadow-lg">
+                <img
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover"
+                  src={project.image}
+                />
+                <div className="relative z-10 w-full h-full border-4 border-primary bg-gray-200 bg-opacity-80 hover:bg-opacity-90 transition duration-300 ease-in-out p-6 rounded-lg shadow-lg opacity-0 hover:opacity-100 flex flex-col justify-center items-center">
+                  <h2 className="text-xs font-semibold tracking-wider text-gray-700 mb-4 uppercase">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="text-xl font-bold text-primary mb-4">
+                    {project.title}
+                  </h1>
+                  <p className="text-gray-600 leading-relaxed text-center">
+                    {project.description}
+                  </p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
