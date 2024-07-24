06b98775ee6483d64ffcf59a9cc167071ac3dd9b
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-24 11:27:40 +0200

    Style Contact component with Tailwind CSS

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -24,30 +24,31 @@ export default function Contact() {
   }
 
   return (
-    <section id="contact" className='map-section'>
-      <div className="container">
-        <div className="map">
+    <section id="contact" className='relative bg-gray-300 font-roboto'>
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-primary rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"
             title="map"
-            className="map-iframe"
+            className="absolute inset-0"
+            style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/place?q=Innsbruck,+Austria&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
-          <div className="map-info">
-            <div className="address">
-              <h2 className="address-title">
+          <div className="bg-primary bg-opacity-90 relative flex flex-wrap py-6 rounded shadow-md w-full">
+            <div className="lg:w-1/2 w-full px-6 mb-4 lg:mb-0">
+              <h2 className="title-font font-semibold text-secondary tracking-widest text-xs text-center">
                 ADDRESS
               </h2>
-              <p className="address-text">
+              <p className="mt-1 text-accent">
                 Innsbruck, Austria
               </p>
             </div>
-            <div className="email">
-              <h2 className="email-title">
+            <div className="lg:w-1/2 w-full px-6 text-center">
+              <h2 className="title-font font-semibold text-secondary tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="email-link" href='mailto:alina.teower@gmail.com'>
+              <a className="text-accent leading-relaxed break-words" href='mailto:alina.teower@gmail.com'>
                 alina.teower@gmail.com
               </a>
             </div>
@@ -57,53 +58,56 @@ export default function Contact() {
           netlify
           name="contact"
           onSubmit={handleSubmit}
-          className="form"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 bg-accent shadow-lg rounded-lg p-8"
         >
-          <h2 className="title">
+          <h2 className="text-primary text-3xl mb-4 font-semibold title-font">
             Get in Touch
           </h2>
-          <p className="description">
+          <p className="leading-relaxed mb-6 text-secondary">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
             suscipit officia aspernatur veritatis.
           </p>
-          <div className="form-group">
-            <label htmlFor="name">
+          <div className="relative mb-6">
+            <label htmlFor="name" className="leading-7 text-sm text-secondary">
               Name
             </label>
             <input
               type="text"
               id="name"
               name="name"
+              className="w-full bg-gray-100 rounded border border-gray-300 focus:border-primary focus:ring-2 focus:ring-primary text-base outline-none text-gray-800 py-2 px-4 transition-colors duration-200 ease-in-out"
               onChange={(e) => setName(e.target.value)}
               placeholder="Your Name"
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="email">
+          <div className="relative mb-6">
+            <label htmlFor="email" className="leading-7 text-sm text-secondary">
               Email
             </label>
             <input
               type="email"
               id="email"
               name="email"
+              className="w-full bg-gray-100 rounded border border-gray-300 focus:border-primary focus:ring-2 focus:ring-primary text-base outline-none text-gray-800 py-2 px-4 transition-colors duration-200 ease-in-out"
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Your Email"
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="message">
+          <div className="relative mb-6">
+            <label htmlFor="message" className="leading-7 text-sm text-secondary">
               Message
             </label>
             <textarea
               id="message"
               name="message"
+              className="w-full bg-gray-100 rounded border border-gray-300 focus:border-primary focus:ring-2 focus:ring-primary h-32 text-base outline-none text-gray-800 py-2 px-4 resize-none leading-6 transition-colors duration-200 ease-in-out"
               onChange={(e) => setMessage(e.target.value)}
               placeholder="Your Message"
             />
           </div>
           <button
             type="submit"
-            className="submit-button">
+            className="text-accent bg-teal-600 border-0 py-3 px-6 focus:outline-none hover:bg-teal-700 rounded-lg text-lg font-medium transition-colors duration-200 ease-in-out">
               Submit
           </button>
         </form>
