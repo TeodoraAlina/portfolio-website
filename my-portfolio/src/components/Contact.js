c8dd1a9cb2cabb65ec8875c9151d1757eabc3f3e
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-24 11:20:38 +0200

    Add basic form structure to Contact component

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,7 +1,86 @@
-import React from 'react'
+import React from 'react';
 
 export default function Contact() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      ).join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
   return (
-    <div>Contact</div>
-  )
+    <section id="contact">
+      <div className="container">
+        <form
+          netlify
+          name="contact"
+          onSubmit={handleSubmit}
+          className="form"
+        >
+          <h2 className="title">
+            Get in Touch
+          </h2>
+          <p className="description">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+            suscipit officia aspernatur veritatis.
+          </p>
+          <div className="form-group">
+            <label htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Your Name"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Your Email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Your Message"
+            />
+          </div>
+          <button
+            type="submit"
+            className="submit-button">
+              Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
