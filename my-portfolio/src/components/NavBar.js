fb9a9484733261780235cb79beeeb910d063a6ed
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-26 14:12:32 +0200

    Add CSS animations to NavBar component

--- my-portfolio/src/components/NavBar.js
+++ my-portfolio/src/components/NavBar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
   };
 
   return (
-    <header className="bg-primary md:sticky top-0 z-10 shadow-lg">
+    <header className="bg-primary bg-opacity-90 md:sticky top-0 z-10 shadow-lg">
       <div className="w-full max-w-7xl mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <div className="flex justify-between w-full md:w-auto">
           <a 
@@ -27,10 +27,10 @@ export default function Navbar() {
         </div>
         <nav className={`md:flex flex-grow items-center text-gray-800 ${isOpen ? 'block' : 'hidden'} md:block`}>
           <div className="flex flex-col md:flex-row md:mr-auto md:ml-4 md:pl-6 md:border-l-2 md:border-accent md:space-x-5 space-y-2 md:space-y-0">
-            <a href="#projects" className="hover:text-accent transition duration-300 ease-in-out">
+            <a href="#projects" className="hover:text-accent transition duration-300 ease-in-out hover-scale">
               Past Work
             </a>
-            <a href="#skills" className="hover:text-accent transition duration-300 ease-in-out">
+            <a href="#skills" className="hover:text-accent transition duration-300 ease-in-out hover-scale">
               Skills
             </a>
           </div>
@@ -38,7 +38,7 @@ export default function Navbar() {
             href="#contact"
             className="inline-flex items-center border-0 py-1 px-3 text-accent focus:outline-none hover:bg-accent rounded hover:text-gray-600 transition duration-300 ease-in-out mt-2 md:mt-0 md:ml-auto">
             Get in Touch
-            <ArrowRightIcon className="w-4 h-4 ml-1" />
+            <ArrowRightIcon className="w-4 h-4 ml-1 animate-arrow" />
           </a>
         </nav>
       </div>
