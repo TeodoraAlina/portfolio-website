ad6bb042cad36e24a76343682301136681159a34
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-28 14:11:38 +0200

    Update colors in Projects component

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -4,21 +4,21 @@ import { CodeIcon } from '@heroicons/react/solid';
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-primary body-font font-roboto bg-gray-200">
+    <section id="projects" className="text-primary body-font font-roboto">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4 text-primary" />
-          <h1 className="sm:text-4xl text-3xl font-extrabold title-font mb-4 text-primary font-roboto">
+          <h1 className="sm:text-4xl text-3xl font-extrabold title-font mb-4 text-darkerPrimary">
             Projects I've Built
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-secondary mt-2">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-darkerSecondary mt-2">
             My projects highlight a range of technical skills and creativity. From a static HTML and CSS website
             for a psychologist, to a Python command line app for restaurant delivery, and full-stack applications 
             like "Smells Like Vacay" for vacation packing and "Cool the Earth" for environmental awareness.
             Each project reflects my diverse skills and evolving expertise.
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-wrap m-4">
           {projects.map((project) => (
             <a
               href={project.link}
@@ -34,7 +34,7 @@ export default function Projects() {
                   <h2 className="text-xs font-semibold tracking-wider text-gray-700 mb-4 uppercase">
                     {project.subtitle}
                   </h2>
-                  <h1 className="text-xl font-bold text-primary mb-4">
+                  <h1 className="text-xl font-bold text-darkerPrimary mb-4">
                     {project.title}
                   </h1>
                   <p className="text-gray-600 leading-relaxed text-center">
@@ -42,7 +42,7 @@ export default function Projects() {
                   </p>
                   <a
                     href={project.github}
-                    className="mt-4 px-6 py-2 text-accent bg-textPrimary border-0 focus:outline-none hover:bg-gray-700 hover:text-white rounded"
+                    className="mt-4 px-6 py-2 text-accent bg-primary border-0 focus:outline-none hover:bg-secondaryButtonHover hover:text-textPrimary rounded"
                   >
                     Github Repository
                   </a>
