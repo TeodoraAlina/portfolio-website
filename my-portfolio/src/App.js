b151072fa357aed04c55d03fa245a28807dfd903
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-28 11:22:16 +0200

    Integrate ThemeProvider in App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -4,18 +4,21 @@ import NavBar from './components/NavBar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   return (
-    <div className="App">
-      <main className='text-textPrimary bg-accent body-font'>
-        <NavBar />
-        <About />
-        <Projects />
-        <Skills />
-        <Contact />
-      </main>
-    </div>
+    <ThemeProvider>
+      <div className="App">
+        <main className='text-textPrimary bg-accent body-font'>
+          <NavBar />
+          <About />
+          <Projects />
+          <Skills />
+          <Contact />
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
