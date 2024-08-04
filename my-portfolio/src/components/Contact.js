b5bad6e9ac8f05b2d3c6cfbace3edbac0a919caa
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-08-04 10:52:08 +0200

    Add translations to ContactForm and Contact components

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import ContactForm from '../forms/ContactForm';
-
+import { useTranslation } from 'react-i18next';
 
 export default function Contact() {
+  const { t } = useTranslation();
+
   return (
-    <section id="contact" className='relative font-roboto'>
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+    <section id="contact" className="relative font-roboto px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto flex flex-col md:flex-row">
         <div className="lg:w-2/3 md:w-1/2 bg-darkerPrimary rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"
-            title="map"
+            title={t('contact.map')}
             className="absolute inset-0"
             style={{ filter: "opacity(0.7)" }}
             src={process.env.REACT_APP_GOOGLE_MAPS_SRC}
@@ -19,17 +21,17 @@ export default function Contact() {
           <div className="bg-darkerPrimary bg-opacity-90 relative flex flex-wrap py-6 rounded shadow-md w-full">
             <div className="lg:w-1/2 w-full px-6 mb-4 lg:mb-0">
               <h2 className="title-font font-semibold text-darkerSecondary tracking-widest text-xs text-center">
-                ADDRESS
+                {t('contact.address')}
               </h2>
               <p className="mt-1 text-secondary">
-                Innsbruck, Austria
+                {t('contact.location')}
               </p>
             </div>
             <div className="lg:w-1/2 w-full px-6 text-center">
               <h2 className="title-font font-semibold text-darkerSecondary tracking-widest text-xs">
-                EMAIL
+                {t('contact.email')}
               </h2>
-              <a className="text-secondary leading-relaxed break-words" href='mailto:alina.teower@gmail.com'>
+              <a className="text-secondary leading-relaxed break-words" href="mailto:alina.teower@gmail.com">
                 alina.teower@gmail.com
               </a>
             </div>
@@ -37,17 +39,16 @@ export default function Contact() {
         </div>
         <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 bg-darkerSecondary shadow-lg rounded-lg p-8">
           <h2 className="text-primary text-3xl mb-4 font-semibold title-font">
-            Get in Touch
+            {t('contact.getInTouch')}
           </h2>
           <p className="leading-relaxed mb-6 text-secondary">
-            If you're interested in working with me, let's talk!
-            I'm excited to explore new opportunities.
+            {t('contact.description')}
           </p>
           <ContactForm />
         </div>
       </div>
       <div className="flex flex-col items-center mt-6 pb-6">
-        <p className="leading-relaxed text-darkerSecondary mb-6">You can also find me on these platforms:</p>
+        <p className="leading-relaxed text-darkerSecondary mb-6">{t('contact.findMeOn')}</p>
         <div className="flex">
           <a href="https://www.linkedin.com/in/alina-teodora-brinzac/" className="text-darkerPrimary mx-2 pr-6">
             <FaLinkedin size={40} />
--- my-portfolio/src/forms/ContactForm.js
+++ my-portfolio/src/forms/ContactForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import { CheckCircleIcon } from '@heroicons/react/solid';
+import { useTranslation } from 'react-i18next';
 
 function ContactForm() {
   const formId = process.env.REACT_APP_FORMSPREE_ID;
@@ -9,11 +10,12 @@ function ContactForm() {
   const [formValues, setFormValues] = useState({ name: '', email: '', message: '' });
   const [buttonText, setButtonText] = useState('Submit');
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const { t } = useTranslation();
 
   const validateField = (name, value) => {
     let error;
     if (!value) {
-      error = `${name} is required`;
+      error = t(`${name}Required`);
     }
     return error;
   };
@@ -21,9 +23,9 @@ function ContactForm() {
   const validateForm = (event) => {
     const { name, email, message } = event.target.elements;
     const errors = {};
-    if (!name.value) errors.name = 'Name is required';
-    if (!email.value) errors.email = 'Email is required';
-    if (!message.value) errors.message = 'Message is required';
+    if (!name.value) errors.name = t('contactForm.nameRequired');
+    if (!email.value) errors.email = t('contactForm.emailRequired');
+    if (!message.value) errors.message = t('contactForm.messageRequired');
     return errors;
   };
 
@@ -33,10 +35,10 @@ function ContactForm() {
     if (Object.keys(formErrors).length === 0) {
       handleSubmit(event).then(() => {
         setFormValues({ name: '', email: '', message: '' });
-        setButtonText('Submitted');
+        setButtonText(t('contactForm.submitted'));
         setShowSuccessMessage(true);
         setTimeout(() => {
-          setButtonText('Submit');
+          setButtonText(t('contactForm.submit'));
           setShowSuccessMessage(false);
         }, 3000);
       });
@@ -61,20 +63,20 @@ function ContactForm() {
     <form onSubmit={onSubmit} className="flex flex-col">
       <div className="relative mb-6">
         <label htmlFor="name" className="leading-7 text-sm text-secondary">
-          Name
+          {t('contactForm.nameLabel')}
         </label>
         <input
           id="name"
           type="text"
           name="name"
-          aria-label="Name"
+          aria-label={t('contactForm.nameLabel')}
           value={formValues.name}
           className={`contact-input w-full bg-gray-100 rounded border ${errors.name ? 'border-red-500' : 'border-gray-300'} focus:border-primary focus:ring-2 focus:ring-primary text-base outline-none text-gray-800 py-2 px-4 transition-colors duration-200 ease-in-out`}
-          placeholder="Your Name"
+          placeholder={t('contactForm.nameLabel')}
           onChange={handleChange}
         />
         <ValidationError 
-          prefix="Name" 
+          prefix={t('contactForm.nameLabel')} 
           field="name"
           errors={state.errors}
           aria-live="assertive"
@@ -83,20 +85,20 @@ function ContactForm() {
       </div>
       <div className="relative mb-6">
         <label htmlFor="email" className="leading-7 text-sm text-secondary">
-          Email Address
+          {t('contactForm.emailLabel')}
         </label>
         <input
           id="email"
           type="email"
           name="email"
-          aria-label="Email"
+          aria-label={t('contactForm.emailLabel')}
           value={formValues.email}
           className={`contact-input w-full bg-gray-100 rounded border ${errors.email ? 'border-red-500' : 'border-gray-300'} focus:border-primary focus:ring-2 focus:ring-primary text-base outline-none text-gray-800 py-2 px-4 transition-colors duration-200 ease-in-out`}
-          placeholder="Your Email"
+          placeholder={t('contactForm.emailLabel')}
           onChange={handleChange}
         />
         <ValidationError 
-          prefix="Email" 
+          prefix={t('contactForm.emailLabel')} 
           field="email"
           errors={state.errors}
           aria-live="assertive"
@@ -105,19 +107,19 @@ function ContactForm() {
       </div>
       <div className="relative mb-6">
         <label htmlFor="message" className="leading-7 text-sm text-secondary">
-          Message
+          {t('contactForm.messageLabel')}
         </label>
         <textarea
           id="message"
           name="message"
-          aria-label="Message"
+          aria-label={t('contactForm.messageLabel')}
           value={formValues.message}
           className={`contact-input w-full bg-gray-100 rounded border ${errors.message ? 'border-red-500' : 'border-gray-300'} focus:border-primary focus:ring-2 focus:ring-primary h-32 text-base outline-none text-gray-800 py-2 px-4 resize-none leading-6 transition-colors duration-200 ease-in-out`}
-          placeholder="Your Message"
+          placeholder={t('contactForm.messageLabel')}
           onChange={handleChange}
         />
         <ValidationError 
-          prefix="Message" 
+          prefix={t('contactForm.messageLabel')} 
           field="message"
           errors={state.errors}
           aria-live="assertive"
@@ -129,20 +131,20 @@ function ContactForm() {
         disabled={state.submitting}
         className="flex items-center justify-center text-darkerSecondary bg-primary border-0 py-3 px-6 focus:outline-none hover:bg-teal-700 rounded-lg text-lg font-medium transition-colors duration-200 ease-in-out"
       >
-        {buttonText === 'Submitted' ? (
+        {buttonText === t('contactForm.submitted') ? (
           <>
             <CheckCircleIcon className="h-5 w-5 text-darkerSecondary mr-2" />
-            <span>Submitted</span>
+            <span>{t('contactForm.submitted')}</span>
           </>
         ) : state.submitting ? (
-          'Submitting...'
+          t('contactForm.submitting')
         ) : (
-          'Submit'
+          t('contactForm.submit')
         )}
       </button>
       {showSuccessMessage && (
         <div className="flex items-center mt-4 text-secondary">
-          <p>Thank you for your message! I will get back to you soon.</p>
+          <p>{t('contactForm.successMessage')}</p>
         </div>
       )}
     </form>
