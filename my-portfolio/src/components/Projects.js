772fb78fcec2efc1c0e656cdc67e17294697d11c
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-08-04 09:55:54 +0200

    Add translations to Projects component

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -4,10 +4,12 @@ import { CodeIcon } from '@heroicons/react/solid';
 import { projects } from '../data/data';
 import useItemAnimations from '../hooks/useItemAnimations';
 import useHeaderAnimation from '../hooks/useHeaderAnimation';
+import { useTranslation } from 'react-i18next';
 
 export default function Projects() {
   const { headerRef, headerAnimation } = useHeaderAnimation();
   const { itemsRef, itemAnimations } = useItemAnimations(projects);
+  const { t } = useTranslation();
 
   return (
     <section id="projects" className="text-primary body-font font-roboto">
@@ -15,20 +17,17 @@ export default function Projects() {
         <animated.div ref={headerRef} style={headerAnimation} className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4 text-darkerSecondary" />
           <h1 className="sm:text-4xl text-3xl font-extrabold title-font mb-4 text-darkerPrimary">
-            Projects I've Built
+            {t('projects.header')}
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-darkerSecondary mt-2">
-            My projects highlight a range of technical skills and creativity. From a static HTML and CSS website
-            for a psychologist, to a Python command line app for restaurant delivery, and full-stack applications 
-            like "Smells Like Vacay" for vacation packing and "Cool the Earth" for environmental awareness.
-            Each project reflects my diverse skills and evolving expertise.
+            {t('projects.description')}
           </p>
         </animated.div>
         <div ref={itemsRef} className="flex flex-wrap m-4">
           {itemAnimations.map((style, index) => (
             <animated.div
               onClick={() => window.location.href = projects[index].link}
-              key={projects[index].image}
+              key={projects[index].key}
               style={style}
               className="sm:w-1/2 w-full p-4 cursor-pointer"
             >
@@ -43,17 +42,17 @@ export default function Projects() {
                     {projects[index].subtitle}
                   </h2>
                   <h1 className="text-xl font-bold text-darkerPrimary mb-4">
-                    {projects[index].title}
+                    {t(`projects.data.${projects[index].key}.title`)}
                   </h1>
                   <p className="text-gray-600 leading-relaxed text-center">
-                    {projects[index].description}
+                    {t(`projects.data.${projects[index].key}.description`)}
                   </p>
                   <a
                     href={projects[index].github}
                     className="mt-4 px-6 py-2 text-accent bg-primary border-0 focus:outline-none hover:bg-secondaryButtonHover hover:text-textPrimary rounded"
                     onClick={(e) => e.stopPropagation()}
                   >
-                    Github Repository
+                    {t('projects.githubRepository')}
                   </a>
                 </div>
               </div>
